aab37e89c16704946fbbf4ad8f5df16d0c8f7c18
Arkay3201423 <3201423@sps-student.sk.ca>
2020-09-29 09:21:48 -0600

    new project of 2d array and perlin noise

---
+++ perline noise/cs30-p5js-template-master/sketch.js
@@ -0,0 +1,34 @@
+// Array Demo
+
+let numberOfRects;
+let rectHeights = [];
+let rectWidth;
+let time = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  numberOfRects = width;
+  rectWidth = width / numberOfRects;
+  setTheHeights();
+}
+
+function draw() {
+  background(220);
+  displayRects();
+}
+
+
+function setTheHeights() {
+  for (let i = 0; i < numberOfRects; i++) {
+    let theHeight = noise(time) * height;
+    rectHeights.push(theHeight);
+    time += 0.01;
+  }
+}
+
+function displayRects() {
+  for (let i=0; i<numberOfRects; i++) {
+    fill("black");
+    rect(i*rectWidth, height-rectHeights[i], rectWidth, rectHeights[i]);
+  }
+}
